97f7b031b7c7fa14d905397a2bbc33610c20a50f
Jan Kurs <kurs@inf.unibe.ch>
2016-08-24 09:49:40 +0200

    folding and unzip without prompt

--- .utilities/loadSources.sh
+++ .utilities/loadSources.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
+
+echo 'travis_fold:start:SOURCES'
 wget http://scg.unibe.ch/download/jk/sources/smalltalk-src-all.zip
 mkdir ${TRAVIS_BUILD_DIR}/smalltalk-src
-unzip smalltalk-src-all.zip -d ${TRAVIS_BUILD_DIR}/smalltalk-src/
+unzip -o smalltalk-src-all.zip -d ${TRAVIS_BUILD_DIR}/smalltalk-src/
+echo 'travis_fold:start:SOURCES'
