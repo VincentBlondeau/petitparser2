d3b3c9d552f5ab5ac1a6ded0a38367b1935960d1
Jan Kurs <kurs@inf.unibe.ch>
2016-08-24 10:06:37 +0200

    debugging

--- .utilities/loadSources.sh
+++ .utilities/loadSources.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-echo 'travis_fold:start:SOURCES'
+echo -en 'travis_fold:start:SOURCES\\r'
 wget http://scg.unibe.ch/download/jk/sources/smalltalk-src-all.zip
 mkdir ${TRAVIS_BUILD_DIR}/smalltalk-src
 unzip -qo smalltalk-src-all.zip -d ${TRAVIS_BUILD_DIR}/smalltalk-src/
-echo 'travis_fold:start:SOURCES'
+echo -en 'travis_fold:start:SOURCES\\r'
